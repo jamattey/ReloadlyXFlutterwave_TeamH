60cbc43df91e3fe23e85954c6adaeef91cc3d190
Kayode Adeniyi <adeniyikayode22@gmail.com>
2021-09-22 06:34:50 +0100

    forms

--- App.js
+++ App.js
@@ -1,43 +1,6 @@
 import React from "react";
-import { useEffect, useState } from "react";
-
-
-// import WelcomeScreen from "./app/screens/WelcomeScreen";
-// import ViewImageScreen from "./app/screens/ViewImageScreen";
-// import Screen from "./app/components/Screen";
-// import Icon from "./app/components/Icon";
-// import ListItem from "./app/components/ListItem";
-// import AccountScreen from "./app/screens/AccountScreen";
-// import ListingsScreen from "./app/screens/ListingsScreen";
-// import MessagesScreen from "./app/screens/MessagesScreen";
-
-// export default function App() {
-//   return <ListingsScreen />;
-// }
-
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
-
-const categories = [
-  { label: "Plastics", value: 1 },
-  { label: "Metals", value: 2 },
-  { label: "Paper", value: 3 },
-];
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput icon="email" placeholder="Email" />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
